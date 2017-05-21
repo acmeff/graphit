3feb4535490b9c8e9f048347176384cadc6a7fa0
Audrey Mefford <acmefford@gmail.com>
2017-05-21 15:24:29 -0700

    Refactor splash into three canvases

--- graphit/frontend/components/splash/canvas.jsx
+++ graphit/frontend/components/splash/canvas.jsx
@@ -15,149 +15,159 @@ class Canvas extends React.Component{
     this.incrementHeight = this.incrementHeight.bind(this);
   }
     componentDidMount(){
-      this.canvas = ReactDOM.findDOMNode(this.refs.splashCanvas);
-      this.ctx = this.canvas.getContext('2d');
+      this.blueCanvas = ReactDOM.findDOMNode(this.refs.splashCanvasBlue);
+      this.blue = this.blueCanvas.getContext('2d');
+      this.blue.translate(0.5, 0.5);
 
-      setTimeout(this.blueLine, 1000);
-      setTimeout(this.bars, 4000);
-      setTimeout(this.purpleLine, 6000);
+      this.orangeCanvas = ReactDOM.findDOMNode(this.refs.splashCanvasOrange);
+      this.orange = this.orangeCanvas.getContext('2d');
+      this.orange.translate(0.5, 0.5);
+
+      this.purpCanvas = ReactDOM.findDOMNode(this.refs.splashCanvasPurp);
+      this.purp = this.purpCanvas.getContext('2d');
+      this.purp.translate(0.5, 0.5);
+
+      this.blueLine();
+      this.purpleLine();
+      this.bars();
     }
 
     purpleLine(){
 
-      this.ctx.translate(0.5, 0.5);
-      this.ctx.lineWidth = 3;
-      this.ctx.lineJoin= 'round';
-      this.ctx.strokeStyle = "#a679bb";
-      this.xStartP = 50;
-      this.yStartP = 300;
-      this.xP = 50;
-      this.yP = 300;
-      this.clear = setInterval(this.incrementPurpUpRight, 1);
+      this.purp.lineWidth = 3;
+      this.purp.lineJoin= 'round';
+      this.purp.fillStyle= "#a679bb";
+      this.purp.strokeStyle = "#a679bb";
+      this.xStartP = 100;
+      this.yStartP = 400;
+      this.xP = 100;
+      this.yP = 400;
+      this.clearPurp = setInterval(this.incrementPurpUpRight, 1);
     }
 
     bars(){
-
-      this.ctx.translate(-0.5, -0.5);
-      this.ctx.lineWidth = 3;
-      this.ctx.lineJoin= 'round';
-      this.ctx.strokeStyle = "#f18e43";
-      this.ctx.fillStyle = '#f18e43';
+      this.orange.lineWidth = 3;
+      this.orange.lineJoin= 'round';
+      this.orange.strokeStyle = "#f18e43";
+      this.orange.fillStyle = '#f18e43';
       this.h = 0;
       this.clearHeight = setInterval(this.incrementHeight, 1);
     }
 
     incrementHeight(){
-      if (this.h >= 450){
+      if (this.h >= 300){
         clearInterval(this.clearHeight);
       } else {
         this.h ++;
-        this.ctx.fillRect(60, 300, 30, 0 + this.h);
-        this.ctx.fillRect(110, 320, 30, 0 + this.h);
-        this.ctx.fillRect(160, 350, 30, 0 + this.h);
-        this.ctx.fillRect(210, 280, 30, 0 + this.h);
-        this.ctx.fillRect(260, 330, 30, 0 + this.h);
-        this.ctx.fillRect(310, 420, 30, 0 + this.h);
-        this.ctx.fillRect(360, 270, 30, 0 + this.h);
-        this.ctx.fillRect(410, 400, 30, 0 + this.h);
+        this.orange.fillRect(150, 250, 60, this.h);
+        this.orange.fillRect(250, 285, 60, this.h);
+        this.orange.fillRect(350, 210, 60, this.h);
+        this.orange.fillRect(450, 390, 60, this.h);
+        this.orange.fillRect(550, 275, 60, this.h);
+        this.orange.fillRect(650, 395, 60, this.h);
+        this.orange.fillRect(750, 220, 60, this.h);
+        this.orange.fillRect(850, 300, 60, this.h);
         this.render();
       }
     }
 
     blueLine(){
 
-      this.ctx.translate(0.5, 0.5);
-      this.ctx.lineWidth = 3;
-      this.ctx.lineJoin= 'round';
-      this.ctx.strokeStyle = "#3ba8f9";
-      this.xStart = 50;
+      this.blue.lineWidth = 3;
+      this.blue.lineJoin= 'round';
+      this.blue.strokeStyle = "#3ba8f9";
+      this.xStart = 100;
       this.yStart = 50;
-      this.x = 50;
-      this.y = 50;
+      this.x = 100;
+      this.y = 20;
       this.clear = setInterval(this.incrementLineDownRight, 1);
     }
 
     incrementLineDownRight(){
 
-      if (this.x > (100 + this.xStart)){
+      if (this.x > (95 + this.xStart)){
         clearInterval(this.clear);
         this.xStart = this.x;
         this.clear = setInterval(this.incrementLineUpRight, 1);
-      } else if(this.x >= 450) {
+      } else if(this.x >= 900) {
         clearInterval(this.clear);
       }
       else {
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.x, this.y);
+        this.blue.beginPath();
+        this.blue.moveTo(this.x, this.y);
         this.x ++;
         this.y ++;
-        this.ctx.lineTo(this.x, this.y);
-        this.ctx.stroke();
+        this.blue.lineTo(this.x, this.y);
+        this.blue.stroke();
         this.render();
       }
     }
 
     incrementPurpUpRight(){
 
-      if (this.xP > (110 + this.xStartP)){
-        clearInterval(this.clear);
+      if (this.xP > (135 + this.xStartP)){
+        clearInterval(this.clearPurp);
         this.xStartP = this.xP;
-        this.clear = setInterval(this.incrementPurpDownRight, 1);
-      } else if(this.xP >= 450) {
-        clearInterval(this.clear);
+        this.clearPurp = setInterval(this.incrementPurpDownRight, 1);
+      } else if(this.xP >= 900) {
+        clearInterval(this.Purp);
       } else {
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.xP, this.yP);
+        this.purp.beginPath();
+        this.purp.moveTo(this.xP, this.yP);
         this.xP ++;
         this.yP --;
-        this.ctx.lineTo(this.xP, this.yP);
-        this.ctx.stroke();
+        this.purp.lineTo(this.xP, this.yP);
+        this.purp.stroke();
         this.render();
       }
     }
 
     incrementPurpDownRight(){
 
-      if (this.xP > (180 + this.xStartP)){
-        clearInterval(this.clear);
+      if (this.xP > (95 + this.xStartP)){
+        clearInterval(this.clearPurp);
         this.xStartP = this.xP;
-        this.clear = setInterval(this.incrementPurpUpRight, 1);
-      } else if(this.xP >= 450) {
-        clearInterval(this.clear);
+        this.clearPurp = setInterval(this.incrementPurpUpRight, 1);
+      } else if(this.xP >= 900) {
+        clearInterval(this.clearPurp);
       }
       else {
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.xP, this.yP);
+        this.purp.beginPath();
+        this.purp.moveTo(this.xP, this.yP);
         this.xP ++;
         this.yP ++;
-        this.ctx.lineTo(this.xP, this.yP);
-        this.ctx.stroke();
+        this.purp.lineTo(this.xP, this.yP);
+        this.purp.stroke();
         this.render();
       }
     }
 
     incrementLineUpRight(){
 
-      if (this.x > (60 + this.xStart)){
+      if (this.x > (110 + this.xStart)){
         clearInterval(this.clear);
         this.xStart = this.x;
         this.clear = setInterval(this.incrementLineDownRight, 1);
-      } else if(this.x >= 450) {
+      } else if(this.x >= 900) {
         clearInterval(this.clear);
       } else {
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.x, this.y);
-        this.x ++;
+        this.blue.beginPath();
+        this.blue.moveTo(this.x, this.y);
+        this.x += 2;
         this.y --;
-        this.ctx.lineTo(this.x, this.y);
-        this.ctx.stroke();
+        this.blue.lineTo(this.x, this.y);
+        this.blue.stroke();
         this.render();
       }
     }
 
     render(){
       return(
-        <canvas height='500' width='500' ref='splashCanvas' className='canvas'></canvas>
+        <section className='canvases'>
+          <canvas height='500' width='1000' ref='splashCanvasBlue' className='canvas blue'></canvas>
+          <canvas height='500' width='1000' ref='splashCanvasOrange' className='canvas orange'></canvas>
+          <canvas height='500' width='1000' ref='splashCanvasPurp' className='canvas purp'></canvas>
+        </section>
       );
     }
 }
