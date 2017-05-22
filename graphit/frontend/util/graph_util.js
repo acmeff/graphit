285432edf37a795744070a7dbf17bdca9fbd13e2
Audrey Mefford <acmefford@gmail.com>
2017-05-21 18:50:12 -0700

    Graph api util ajax methods

---
+++ graphit/frontend/util/graph_util.js
@@ -0,0 +1,28 @@
+export const createGraph = graph => (
+  $.ajax({
+    url: 'api/graphs',
+    method: 'POST',
+    data: { graph }
+  })
+);
+
+export const deleteGraph = graph => (
+  $.ajax({
+    url: `api/graphs/${graph.id}`,
+    method: 'DELETE'
+  })
+);
+
+export const getGraph = (graphId) => (
+  $.ajax({
+    url: `api/graphs/${graphId}`,
+    method: 'GET'
+  })
+);
+
+export const getGraphs = () => (
+  $.ajax({
+    url: `api/graphs`,
+    method: 'GET'
+  })
+);
