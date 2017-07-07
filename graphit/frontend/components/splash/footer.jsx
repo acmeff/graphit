165a17d6078ca90d634e1e6442b9fd981178fede
Audrey Mefford <acmefford@gmail.com>
2017-07-07 15:30:46 -0700

    Turn footer lis into links

--- graphit/frontend/components/splash/footer.jsx
+++ graphit/frontend/components/splash/footer.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const Footer = () => (
   <footer>
     <ul>
-      <li>LinkedIn</li>
-      <li>GitHub</li>
-      <li>Portfolio</li>
+      <li><a target='_blank' href='https://www.linkedin.com/in/audrey-mefford/'>LinkedIn</a></li>
+      <li><a target='_blank' href='https://github.com/acmeff'>GitHub</a></li>
+      <li><a target='_blank' href='http://audreymefford.com'>Portfolio</a></li>
     </ul>
   </footer>
 );
