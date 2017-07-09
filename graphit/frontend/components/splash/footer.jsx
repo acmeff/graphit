d5a7591f34eccfbd7a671bc6d5af218989b28b21
Audrey Mefford <acmefford@gmail.com>
2017-07-09 13:56:31 -0700

    Rearrange footer links

--- graphit/frontend/components/splash/footer.jsx
+++ graphit/frontend/components/splash/footer.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const Footer = () => (
   <footer>
     <ul>
+      <li>Created by: Audrey Mefford</li>
       <li><a target='_blank' href='https://www.linkedin.com/in/audrey-mefford/'><img src='assets/linkedin.png'/>LinkedIn</a></li>
       <li><a target='_blank' href='https://github.com/acmeff'><img src='assets/github.png'/>GitHub</a></li>
       <li><a target='_blank' href='http://audreymefford.com'><img src='assets/portfolio.png'/>Portfolio</a></li>
