25eef4e168dd3d984ffca64f17b4fbd113de7ec8
Audrey Mefford <acmefford@gmail.com>
2017-07-09 13:53:24 -0700

    Fix email link

--- graphit/frontend/components/splash/footer.jsx
+++ graphit/frontend/components/splash/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
       <li><a target='_blank' href='https://www.linkedin.com/in/audrey-mefford/'><img src='assets/linkedin.png'/>LinkedIn</a></li>
       <li><a target='_blank' href='https://github.com/acmeff'><img src='assets/github.png'/>GitHub</a></li>
       <li><a target='_blank' href='http://audreymefford.com'><img src='assets/portfolio.png'/>Portfolio</a></li>
-      <li><a target='_blank' href='http://audreymefford.com'><img src='assets/email.png'/>Email</a></li>
+      <li><a href='mailto:acmefford@gmail.com'><img src='assets/email.png'/>Email</a></li>
     </ul>
 
   </footer>
